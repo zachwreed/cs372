0066a491f99b80a741ad400a3b4cf8ba9ed228ee
Zach Reed <zacwreed@gmail.com>
2020-02-08 23:31:50 -0600

    working

--- chatclient.c
+++ chatclient.c
@@ -7,9 +7,11 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <math.h>
+#include <errno.h>
 
 #define MSGMAX 504
 #define INMAX 500
+#define HANDLE 16
 
 /***********************************************
 ** Function: Error Handler
@@ -26,18 +28,32 @@ void error(const char *msg) {
 ** Prerequisite: buffer is memset to null, bSize contains positive integer, flag contains positive integer. Called within child of spawnPid
 ************************************************/
 int recvMsg(int connectionFD, char* buffer, int bSize, int flag) {
-	int charsRead, charsTotal = 0;
+	int charsRead, charsTotal, count = 0;
+	int size = -1;
+	char * ptr;
+
 	// While buffer doesn't contain endline
-	while(!strcspn(buffer, "\n")) {
+	while(size != charsTotal) {
 		charsRead = recv(connectionFD, buffer + charsTotal, bSize, flag);
-		if (charsRead <= 0) {
-			error("ERROR reading from socket");
-			break;
+		printf("Server: %s\n", buffer);
+		if (charsRead < 0) {
+			printf("ERROR reading from socket %d\n", charsRead);
+			perror("Error from reading:");
+			exit(1);
+		}
+		if (count == 0) {
+			ptr = strchr(buffer, ' ');
+			size = atoi(buffer);
+			*ptr++ = '\0';
+			count++;
+			printf("Server: %s\n", ptr);
 		}
 		charsTotal += charsRead;	// add to offset next receive
 	}
+	printf("Server: %s\n", ptr);
 	return charsTotal;
 }
+
 
 /***********************************************
 ** Function: Send Message
@@ -60,6 +76,15 @@ int sendMsg(int connectionFD, char* buffer, int flag) {
 	return charsTotal;
 }
 
+char* getHandle() {
+	char *handle = malloc(sizeof(char) * HANDLE);
+
+	printf("Enter a handle up to 15 characters: ");
+	fgets(handle, sizeof(handle) - 1, stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
+	handle[strcspn(handle, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+	return handle;
+}
+
 /***********************************************
 ** Function: Main
 ** Prerequisite: args[] contains:
@@ -73,6 +98,7 @@ int main(int argc, char *argv[])
 	struct hostent* serverHostInfo;
 	char buffer[INMAX]; 
 	char msg[MSGMAX]; 	// buffer for <num of bytes> + ' ' <msg>
+	char *handle = malloc(sizeof(char) * HANDLE);
 	if (argc < 3) { 
 		fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); 
 		exit(0); 
@@ -96,30 +122,86 @@ int main(int argc, char *argv[])
 	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
 		error("CLIENT: ERROR connecting");
 
-	// Get input message from user
-	printf("CLIENT: Enter text to send to the server, and then hit enter: ");
-	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
-	memset(msg, '\0', sizeof(msg));
-	fgets(buffer, sizeof(buffer) - 1, stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
-	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
-    int size = round(log10(strlen(buffer)))+2;
-    char len[size];
-	sprintf(len, "%ld", strlen(buffer)+size);
-	strcat(len, " ");
-    strcat(msg, len);
-	strcat(msg, buffer);
+	// Get Handle from User
+	handle = getHandle();
+	int quit = 0;
+	while(quit != 1) {
+		printf("%s> ", handle);
+		memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
+		memset(msg, '\0', sizeof(msg));
+		fgets(buffer, sizeof(buffer) - 1, stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
+		buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+		
+		if (strcmp(buffer, "\\quit") == 0) {
+			quit = 1;
+		} 
+		
+		int size = floor(log10(abs((int)strlen(buffer))))+2;
+		char len[size];
+		sprintf(len, "%ld", strlen(buffer)+size);
+		strcat(len, " ");
+		// printf("Size: %d\n", size);
+		strcat(msg, len);
+		strcat(msg, buffer);
 
-	// Send message to server
-	charsWritten = send(socketFD, msg, strlen(msg), 0); // Write to the server
-	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
-	if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+		charsWritten = 0;
+		int charsTotal = 0;
+		// block until all data is sent
+		while (charsTotal < strlen(msg)) {
+			charsWritten = send(socketFD, msg + charsTotal, strlen(msg), 0);
+			if (charsWritten < 0) {
+				error("CLIENT: ERROR writing to socket");
+			}
+			if (charsWritten == 0) {
+				break;
+			}
+			charsTotal += charsWritten; // add to offset next send
+		}
 
-	// Get return message from server
-	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
-	charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
-	if (charsRead < 0) error("CLIENT: ERROR reading from socket");
-	printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
+		if(quit == 1) {
+			break;
+		}
 
+		// // Send message to server
+		// charsWritten = send(socketFD, msg, strlen(msg), 0); // Write to the server
+		// if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+		// if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+
+		// Get return message from server
+		memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
+		
+		int count = 0;
+		charsRead = 0;
+		charsTotal = 0;
+		int sizeB = -1;
+		char * ptr;
+
+		// Receive message from server
+		while(sizeB != charsTotal) {
+			charsRead = recv(socketFD, buffer + charsTotal, sizeof(buffer)-1, 0);
+			if (charsRead < 0) {
+				perror("Error reading from socket");
+				exit(1);
+			}
+			if (count == 0) {
+				ptr = strchr(buffer, ' ');
+				sizeB = atoi(buffer);
+				*ptr++ = '\0';
+				count++;
+			}
+			charsTotal += charsRead;	// add to offset next receive
+		}
+		if (strcmp(ptr, "\\quit") == 0) {
+			quit = 1;
+		}
+		else { 
+			printf("Serv> %s\n", ptr);
+		}
+			
+		// charsRead = recvMsg(socketFD, buffer, MSGMAX, 0);
+		// if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+		// printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
+	}
 	close(socketFD); // Close the socket
 	return 0;
 }
--- chatserve.py
+++ chatserve.py
@@ -15,35 +15,50 @@ def main():
     port = int(sys.argv[1])
 
     # bind socket to port
-    s.bind( ('', port) )
+    s.bind(('', port))
 
     # put socket on listen
     s.listen(5)
 
+    quit = False
     while True:
         connection, addr = s.accept()
         print("Connected to", addr)
-        msg = ""
-        count = 0
-        size = 0
-        while True:
-            data = connection.recv(CHARMAX)
-            msg += str(data.decode())
-            print("Client loop> " + msg)
 
-            if count == 0:
-                size = int(msg.partition(" ")[0])
-                count += 1
+        while not quit:
+            msg = ""
+            count = 0
+            size = 0
+            
+            # Receive message from client
+            while True:
+                data = connection.recv(CHARMAX)
+                msg += str(data.decode())
+                # print("Client loop> " + msg)
 
-            if size == len(msg):
-                break
-        
-        msg = msg.partition(" ")[2]
-        print("Client> " + msg)
-        buffer = input("You> ")
-        size = round(math.log(len(buffer),10))+2
-        buffer = str(size + len(buffer)) + " " + buffer
-        connection.sendall(buffer.encode())
+                if count == 0:
+                    size = int(msg.partition(" ")[0])
+                    count += 1
+
+                if size == len(msg):
+                    break
+            
+            msg = msg.partition(" ")[2]
+            print("Client> " + msg)
+
+            # Prompt user for response
+            buffer = input("You> ")
+
+            # if input is to quit connection
+            if buffer == "\quit":
+                quit = True
+
+            size = math.floor(math.log(len(buffer),10))+2
+            buffer = str(size + len(buffer)) + " " + buffer
+
+            # Send message to client
+            connection.sendall(buffer.encode())
+
         connection.close()
 
 main()
